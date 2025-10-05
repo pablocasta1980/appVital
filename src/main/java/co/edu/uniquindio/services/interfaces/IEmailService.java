72dd35d60661a83806f92d7ef91840ebde61ffda
pablocasta1980 <pacastanog@uqvirtual.edu.co>
2025-10-05 13:23:38 -0500

    sobre escribir rama remota
    actualizando usuario

--- src/main/java/co/edu/uniquindio/services/interfaces/IEmailService.java
+++
@@ -1,8 +0,0 @@
-package co.edu.uniquindio.services.interfaces;
-
-import co.edu.uniquindio.dto.email.EmailDTO;
-
-public interface IEmailService {
-
-    void enviarCorreo(EmailDTO emailDTO) throws Exception;
-}
